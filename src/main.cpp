980297f04492081740ee82bc2447a32f7540464d
Kurtt806 <93168474+Kurtt806@users.noreply.github.com>
2023-04-27 21:43:03 +0700

    modified:   .pio/build/esp07s/.sconsign39.dblite 	modified:   .pio/build/esp07s/firmware.bin 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,29 @@
  * SERIAL SPEED = 115200
  * UPLOAD SPEED = 921600
  * FRAMEWORK    = arduino
+ * 
+ * _____BOARD_1_____
+ * #define MANAGER_WIFI_NAME_AP "KUR_BOARD_63636"
+ * #define MANAGER_WIFI_PASS_AP "63636kur"
+ * _____BOARD_2_____
+ * #define MANAGER_WIFI_NAME_AP "KUR_BOARD_79574"
+ * #define MANAGER_WIFI_PASS_AP "79574kur"
+ * _____BOARD_3_____
+ * #define MANAGER_WIFI_NAME_AP "KUR_BOARD_36373"
+ * #define MANAGER_WIFI_PASS_AP "36373kur"
+ * _____BOARD_4_____
+ * 
+ *  
+ * _____BOARD_5_____
+ * 
+ * 
  */
 //*****************************************
 
 //*---THAY ĐỔI SSID VÀ PASS MANAGER--------
 
-
-
-#define MANAGER_WIFI_NAME_AP "kur12345"
-#define MANAGER_WIFI_PASS_AP "12345678"
+#define MANAGER_WIFI_NAME_AP "KUR_BOARD_36373"
+#define MANAGER_WIFI_PASS_AP "36373kur"
 
 //*---THAY ĐỔI THÔNG SỐ SERVO--------------
 
